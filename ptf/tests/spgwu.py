e2a25e03f3b4d3293723f41395607869e2ac637e
Badhrinath Pa <badhrinath.pa@gmail.com>
2020-04-15 01:19:15 -0400

    ptf test downlink

--- ptf/tests/spgwu.py
+++ ptf/tests/spgwu.py
@@ -40,6 +40,7 @@ IP_VERSION_4 = 4
 out_ipv4_src = '192.168.0.202'
 out_ipv4_dst = '10.92.1.164'
 src_teid = 101
+dst_teid = 1201
 SPGW_DIR_UNKNOWN = 0;
 SPGW_DIR_UPLINK = 1;
 SPGW_DIR_DOWNLINK = 2;
@@ -67,7 +68,7 @@ PKT_SRC_IP = None
 PKT_DST_IP = None
 
 @group("gtpu")
-class GTPU_far_Test(P4RuntimeTest):
+class GTPU_far_UPLINK_Test(P4RuntimeTest):
     """Tests GTPU routing"""
 
     inner_pkt = None
@@ -188,3 +189,95 @@ class GTPU_far_Test(P4RuntimeTest):
         print self.port1
         testutils.send_packet(self, self.port1, str(pkt))
         testutils.verify_packet(self, exp_pkt, self.port2)
+
+class GTPU_far_DOWNLINK_Test(P4RuntimeTest):
+    """Tests GTPU routing"""
+
+    outer_pkt = None
+    def make_gtp(self,msg_len, teid, flags=0x30, msg_type=0xff):
+        """Convenience function since GTP header has no scapy support"""
+        return struct.pack(">BBHL", flags, msg_type, msg_len, teid)
+
+    def pkt_add_gtp(self,pkt, out_ipv4_src, out_ipv4_dst, teid):
+        payload_ether_frame = pkt[Ether].payload
+        return Ether(src=pkt[Ether].dst, dst=SWITCH1_MAC) / \
+               IP(src=out_ipv4_dst, dst=out_ipv4_src, tos=0,
+                  id=0x1513, flags=0, frag=0) / \
+               UDP(sport=UDP_GTP_DST_PORT, dport=UDP_GTP_DST_PORT, chksum=0) / \
+               self.make_gtp(len(pkt), dst_teid) / \
+               payload_ether_frame
+
+
+    def runTest(self):
+        # Test with different type of packets.
+        for pkt_type in ["udp"]:
+              print_inline("%s ... " % pkt_type)
+              pkt = getattr(testutils, "simple_%s_packet" % pkt_type)()
+              self.inner_pkt = pkt
+              self.testPacket(pkt)
+
+    @autocleanup
+    def testPacket(self, pkt):
+        next_hop_mac = SWITCH1_MAC
+
+        # Add entry to "source_iface_lookup" table. 
+       
+        self.insert(self.helper.build_table_entry(
+                   table_name="IngressPipeImpl.source_iface_lookup",
+                   match_fields={
+                   # Exact match.
+                     "std_meta.ingress_port": self.port2
+                },
+                action_name="IngressPipeImpl.set_source_iface_type",
+                action_params={"src_iface_type":SPGW_IFACE_TYPE_CORE}
+                ))
+
+        # Add entry to pdr_rule_lookup
+        UDP_SRC_PORT = pkt[UDP].sport
+        UDP_DST_PORT = pkt[UDP].dport
+        PKT_SRC_IP = pkt[IP].src
+        PKT_DST_IP = pkt[IP].dst
+        
+        self.insert(self.helper.build_table_entry(
+            table_name="IngressPipeImpl.pdrs",
+            match_fields={
+            # Exact match.
+            "ue_addr": (PKT_DST_IP, IP_MASK),
+            "inet_addr": (PKT_SRC_IP, IP_MASK),
+            "ue_l4_port":(UDP_DST_PORT, UDP_DST_PORT),
+            "inet_l4_port":(UDP_SRC_PORT, UDP_SRC_PORT),
+            "ip_proto":(IP_PROTO_UDP, 0xFF)
+        },
+        action_name="IngressPipeImpl.set_pdr_id",
+        action_params={"id":DL_PDR_ID},
+        priority = 1
+        ))
+        
+        self.insert(self.helper.build_table_entry(
+                   table_name="IngressPipeImpl.fars",
+                   match_fields={
+                   # Exact match.
+                     "local_meta.pdr_id": DL_PDR_ID,
+                },
+                action_name="IngressPipeImpl.set_far_id",
+                action_params={"id":DL_FAR_ID}
+                ))
+
+        self.insert(self.helper.build_table_entry(
+               table_name="IngressPipeImpl.execute_far",
+               match_fields={
+                   # Exact match.
+                     "local_meta.far_id": DL_FAR_ID
+                },
+                action_name="IngressPipeImpl.gtpu_encap_and_forward",
+                action_params={"encap_src_addr":out_ipv4_dst, "encap_dst_addr":out_ipv4_src,"nexthop_mac":next_hop_mac,"outport":self.port1}
+                ))
+
+
+        # Expected pkt should have routed MAC addresses and decremented hop
+        # limit (TTL).
+        exp_pkt = pkt.copy()
+        pkt_decrement_ttl(exp_pkt)
+        outer_pkt = self.pkt_add_gtp(exp_pkt, out_ipv4_src, out_ipv4_dst, dst_teid) 
+        testutils.send_packet(self, self.port2, str(pkt))
+        testutils.verify_packet(self, outer_pkt, self.port1)
