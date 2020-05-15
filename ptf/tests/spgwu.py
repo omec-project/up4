fdb3ec5341be5331163d693d69f7504d93d33fdb
Robert MacDavid <robertmacdavid@gmail.com>
2020-05-15 16:00:55 -0400

    Update ptf/tests/spgwu.py
    Co-authored-by: Carmelo Cascone <carmelo@opennetworking.org>

--- ptf/tests/spgwu.py
+++ ptf/tests/spgwu.py
@@ -286,7 +286,7 @@ class GtpuUplinkDhcpTest(GtpuBaseTest):
 
         self.add_global_session(n4_ip=exp_pkt[IP].src, smf_ip=exp_pkt[IP].dst,
                                 smf_mac=exp_pkt[Ether].dst, smf_port=self.port2,
-                                n4_teid=exp_pkt[gtp.GTP_U_Header].teid, dhcp_req_ctr_id=ctr_id)
+                                n4_teid=exp_pkt[gtp.GTP_U_Header].teid, dhcp_req_ctr_id=ctr_id,)
 
         # read pre and post-QoS packet and byte counters
         self.read_pdr_counters(ctr_id)
