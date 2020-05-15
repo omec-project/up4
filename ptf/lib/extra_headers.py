0cb8f8bcfea752773081911569902562c5dedecf
Robert MacDavid <robertmacdavid@gmail.com>
2020-05-15 16:00:44 -0400

    Update ptf/lib/extra_headers.py
    Co-authored-by: Carmelo Cascone <carmelo@opennetworking.org>

--- ptf/lib/extra_headers.py
+++ ptf/lib/extra_headers.py
@@ -5,7 +5,7 @@ from scapy.layers.l2 import Ether
 
 class CpuHeader(Packet):
     name = "CpuHeader"
-    fields_desc = [BitField("port_num", 0, 9), BitField("padding", 0, 7)]
+    fields_desc = [BitField("port_num", 0, 9), BitField("padding", 0, 7),]
 
 
 bind_layers(CpuHeader, Ether)
