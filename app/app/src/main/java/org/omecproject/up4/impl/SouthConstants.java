8a568a6454c30f78bc3c71e1378d6bcba1baea59
Robert MacDavid <robertmacdavid@gmail.com>
2020-07-28 16:18:31 -0400

    Port app southbound to TNA

--- app/app/src/main/java/org/omecproject/up4/impl/SouthConstants.java
+++ app/app/src/main/java/org/omecproject/up4/impl/SouthConstants.java
@@ -12,20 +12,26 @@ import org.onosproject.net.pi.model.PiTableId;
 
 public final class SouthConstants {
 
+    public static final int INTERFACE_ACCESS = 1;
+    public static final int INTERFACE_CORE = 1;
+
+    public static final int DIRECTION_UPLINK = 1;
+    public static final int DIRECTION_DOWNLINK = 2;
+
     // Counters
     public static final PiCounterId INGRESS_COUNTER_ID = PiCounterId.of("FabricIngress.spgw_ingress.pdr_counter");
     public static final PiCounterId EGRESS_COUNTER_ID = PiCounterId.of("FabricEgress.spgw_egress.pdr_counter");
     // Table names
     //   interface lookup
-    public static final PiTableId IFACE_UPLINK_TBL = PiTableId.of("FabricIngress.spgw_ingress.uplink_filter_table");
-    public static final PiTableId IFACE_DOWNLINK_TBL = PiTableId.of("FabricIngress.spgw_ingress.downlink_filter_table");
+    public static final PiTableId INTERFACE_LOOKUP = PiTableId.of("FabricIngress.spgw_ingress.interface_lookup");
     //   pdr tables
     public static final PiTableId PDR_UPLINK_TBL = PiTableId.of("FabricIngress.spgw_ingress.uplink_pdr_lookup");
     public static final PiTableId PDR_DOWNLINK_TBL = PiTableId.of("FabricIngress.spgw_ingress.downlink_pdr_lookup");
     //   far tables
     public static final PiTableId FAR_TBL = PiTableId.of("FabricIngress.spgw_ingress.far_lookup");
     // Action names
-    public static final PiActionId NO_ACTION = PiActionId.of("nop");
+    public static final PiActionId SET_SOURCE_IFACE = PiActionId.of("FabricIngress.spgw_ingress.set_source_iface");
+    //public static final PiActionId NO_ACTION = PiActionId.of("nop");
     public static final PiActionId LOAD_PDR = PiActionId.of("FabricIngress.spgw_ingress.set_pdr_attributes");
     public static final PiActionId LOAD_FAR_NORMAL =
             PiActionId.of("FabricIngress.spgw_ingress.load_normal_far_attributes");
@@ -33,8 +39,8 @@ public final class SouthConstants {
             PiActionId.of("FabricIngress.spgw_ingress.load_tunnel_far_attributes");
     // Match key names
     //   interface lookup
-    public static final PiMatchFieldId IFACE_UPLINK_KEY = PiMatchFieldId.of("gtp_ipv4_dst");
-    public static final PiMatchFieldId IFACE_DOWNLINK_KEY = PiMatchFieldId.of("ipv4_prefix");
+    public static final PiMatchFieldId GTPU_IS_VALID = PiMatchFieldId.of("gtpu_is_valid");
+    public static final PiMatchFieldId IPV4_DST_ADDR = PiMatchFieldId.of("ipv4_dst_addr");
     //   pdrs
     public static final PiMatchFieldId UE_ADDR_KEY = PiMatchFieldId.of("ue_addr");
     public static final PiMatchFieldId TEID_KEY = PiMatchFieldId.of("teid");
@@ -43,15 +49,20 @@ public final class SouthConstants {
     public static final PiMatchFieldId FAR_ID_KEY = PiMatchFieldId.of("far_id");
     // Action parameter names
     //   interface lookup
+    public static final PiActionParamId SRC_IFACE_PARAM = PiActionParamId.of("src_iface");
+    public static final PiActionParamId DIRECTION_PARAM = PiActionParamId.of("direction");
+    public static final PiActionParamId SKIP_SPGW_PARAM = PiActionParamId.of("skip_spgw");
     //   pdrs
     public static final PiActionParamId CTR_ID = PiActionParamId.of("ctr_id");
     public static final PiActionParamId FAR_ID_PARAM = PiActionParamId.of("far_id");
+    public static final PiActionParamId NEEDS_GTPU_DECAP_PARAM = PiActionParamId.of("needs_gtpu_decap");
     //   fars
     public static final PiActionParamId DROP_FLAG = PiActionParamId.of("drop");
     public static final PiActionParamId NOTIFY_FLAG = PiActionParamId.of("notify_cp");
     public static final PiActionParamId TEID_PARAM = PiActionParamId.of("teid");
     public static final PiActionParamId TUNNEL_SRC_PARAM = PiActionParamId.of("tunnel_src_addr");
     public static final PiActionParamId TUNNEL_DST_PARAM = PiActionParamId.of("tunnel_dst_addr");
+    public static final PiActionParamId TUNNEL_DST_PORT_PARAM = PiActionParamId.of("tunnel_src_port");
     // hide default constructor
     private SouthConstants() {
     }
