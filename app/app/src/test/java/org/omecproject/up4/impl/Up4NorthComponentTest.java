69f3fd4da48ef53b8190346b08dac30050268004
Robert MacDavid <robertmacdavid@gmail.com>
2020-08-19 16:33:15 -0400

    insertion and deletion tests

--- app/app/src/test/java/org/omecproject/up4/impl/Up4NorthComponentTest.java
+++ app/app/src/test/java/org/omecproject/up4/impl/Up4NorthComponentTest.java
@@ -5,7 +5,13 @@ import com.google.rpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.junit.Before;
 import org.junit.Test;
+import org.omecproject.up4.UpfProgrammable;
+import org.omecproject.up4.behavior.TestConstants;
+import org.omecproject.up4.behavior.Up4TranslatorImpl;
 import org.onosproject.net.pi.model.PiPipeconf;
+import org.onosproject.net.pi.runtime.PiTableEntry;
+import org.onosproject.p4runtime.ctl.codec.CodecException;
+import org.onosproject.p4runtime.ctl.codec.Codecs;
 import org.onosproject.p4runtime.ctl.utils.PipeconfHelper;
 import p4.config.v1.P4InfoOuterClass;
 import p4.v1.P4RuntimeOuterClass;
@@ -13,6 +19,7 @@ import p4.v1.P4RuntimeOuterClass;
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -21,7 +28,9 @@ public class Up4NorthComponentTest {
 
     private final Up4NorthComponent up4NorthComponent = new Up4NorthComponent();
     private final Up4NorthComponent.Up4NorthService up4NorthService = up4NorthComponent.up4NorthService;
+    PiPipeconf pipeconf;
     private P4InfoOuterClass.P4Info p4Info;
+    UpfProgrammable upfProgrammable;
 
     static class MockStreamObserver<T> implements StreamObserver<T> {
         public List<T> responsesObserved = new ArrayList<>();
@@ -48,10 +57,115 @@ public class Up4NorthComponentTest {
 
     @Before
     public void setUp() throws Exception {
-        PiPipeconf pipeconf = Up4NorthComponent.buildPipeconf();
+        pipeconf = Up4NorthComponent.buildPipeconf();
         p4Info = PipeconfHelper.getP4Info(pipeconf);
         up4NorthComponent.pipeconf = pipeconf;
         up4NorthComponent.p4Info = p4Info;
+        up4NorthComponent.up4Translator = new Up4TranslatorImpl();
+        up4NorthComponent.up4Service = new MockUp4Service();
+        upfProgrammable = up4NorthComponent.up4Service.getUpfProgrammable();
+    }
+
+    private void insertionTest(PiTableEntry entryToInsert) {
+        MockStreamObserver<P4RuntimeOuterClass.WriteResponse> responseObserver = new MockStreamObserver<>();
+        P4RuntimeOuterClass.Entity entity;
+        try {
+            entity = Codecs.CODECS.entity().encode(entryToInsert, null, pipeconf);
+        } catch (CodecException e) {
+            fail("Unable to encode UP4 entry into a p4runtime entity.");
+            return;
+        }
+
+        P4RuntimeOuterClass.WriteRequest request = P4RuntimeOuterClass.WriteRequest.newBuilder()
+                .addUpdates(P4RuntimeOuterClass.Update.newBuilder()
+                        .setEntity(entity)
+                        .setType(P4RuntimeOuterClass.Update.Type.INSERT)
+                        .build())
+                .build();
+
+        up4NorthService.write(request, responseObserver);
+
+        var response = responseObserver.lastResponse();
+        assertThat(response, equalTo(P4RuntimeOuterClass.WriteResponse.getDefaultInstance()));
+    }
+
+    private void deletionTest(PiTableEntry entryToDelete) {
+        MockStreamObserver<P4RuntimeOuterClass.WriteResponse> responseObserver = new MockStreamObserver<>();
+        P4RuntimeOuterClass.Entity entity;
+        try {
+            entity = Codecs.CODECS.entity().encode(entryToDelete, null, pipeconf);
+        } catch (CodecException e) {
+            fail("Unable to encode UP4 entry into a p4runtime entity.");
+            return;
+        }
+
+        P4RuntimeOuterClass.WriteRequest request = P4RuntimeOuterClass.WriteRequest.newBuilder()
+                .addUpdates(P4RuntimeOuterClass.Update.newBuilder()
+                        .setEntity(entity)
+                        .setType(P4RuntimeOuterClass.Update.Type.DELETE)
+                        .build())
+                .build();
+
+        up4NorthService.write(request, responseObserver);
+
+        var response = responseObserver.lastResponse();
+        assertThat(response, equalTo(P4RuntimeOuterClass.WriteResponse.getDefaultInstance()));
+    }
+
+
+
+    @Test
+    public void downlinkFarTest() {
+        PiTableEntry entry = TestConstants.UP4_DOWNLINK_FAR;
+        insertionTest(entry);
+        assertThat(upfProgrammable.getInstalledFars().size(), equalTo(1));
+        deletionTest(entry);
+        assertTrue(upfProgrammable.getInstalledFars().isEmpty());
+    }
+
+    @Test
+    public void uplinkFarTest() {
+        PiTableEntry entry = TestConstants.UP4_UPLINK_FAR;
+        insertionTest(entry);
+        assertThat(upfProgrammable.getInstalledFars().size(), equalTo(1));
+        deletionTest(entry);
+        assertTrue(upfProgrammable.getInstalledFars().isEmpty());
+    }
+
+    @Test
+    public void downlinkPdrTest() {
+        PiTableEntry entry = TestConstants.UP4_DOWNLINK_PDR;
+        insertionTest(entry);
+        assertThat(upfProgrammable.getInstalledPdrs().size(), equalTo(1));
+        deletionTest(entry);
+        assertTrue(upfProgrammable.getInstalledPdrs().isEmpty());
+    }
+
+    @Test
+    public void uplinkPdrTest() {
+        PiTableEntry entry = TestConstants.UP4_UPLINK_PDR;
+        insertionTest(entry);
+        assertThat(upfProgrammable.getInstalledPdrs().size(), equalTo(1));
+        deletionTest(entry);
+        assertTrue(upfProgrammable.getInstalledPdrs().isEmpty());
+    }
+
+    @Test
+    public void downlinkInterfaceTest() {
+        PiTableEntry entry = TestConstants.UP4_DOWNLINK_INTERFACE;
+        insertionTest(entry);
+        assertThat(upfProgrammable.getInstalledInterfaces().size(), equalTo(1));
+        deletionTest(entry);
+        assertTrue(upfProgrammable.getInstalledInterfaces().isEmpty());
+    }
+
+    @Test
+    public void uplinkInterfaceTest() {
+        PiTableEntry entry = TestConstants.UP4_UPLINK_INTERFACE;
+        insertionTest(entry);
+        assertThat(upfProgrammable.getInstalledInterfaces().size(), equalTo(1));
+        deletionTest(entry);
+        assertTrue(upfProgrammable.getInstalledInterfaces().isEmpty());
     }
 
 
