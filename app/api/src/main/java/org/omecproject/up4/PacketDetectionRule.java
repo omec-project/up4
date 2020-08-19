864e826f2b5f77f4e0f9a67e9efb195f18b149a7
Robert MacDavid <robertmacdavid@gmail.com>
2020-08-19 11:43:59 -0400

    missing constant

--- app/api/src/main/java/org/omecproject/up4/PacketDetectionRule.java
+++ app/api/src/main/java/org/omecproject/up4/PacketDetectionRule.java
@@ -29,6 +29,8 @@ public final class PacketDetectionRule {
     private final Type type; // Is the PDR Uplink, Downlink, etc.
     private Integer globalFarId; // The non-session-local ID of the FAR that should apply to packets if this PDR hits
 
+    private static final int SESSION_ID_BITWIDTH = 96;
+
     private PacketDetectionRule(ImmutableByteSequence sessionId, Integer ctrId, Integer localFarId, Ip4Address ueAddr,
                                 ImmutableByteSequence teid, Ip4Address tunnelDst, Integer globalFarId, Type type) {
         this.ueAddr = ueAddr;
