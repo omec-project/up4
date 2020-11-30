691b6ce7e50292c39877609ac7a0a87b4e621482
Robert MacDavid <robert@opennetworking.org>
2020-11-30 16:16:57 -0500

    pfcp smoke test working

--- scenarios/topo/pfcp-entry.sh
+++ scenarios/topo/pfcp-entry.sh
@@ -4,4 +4,4 @@
 #
 # SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
 
-/bin/pfcpiface -config /opt/bess/bessctl/conf/upf.json -n4SrcIPStr 0.0.0.0 -p4RtcServerIP onos -p4RtcServerPort 51001
+/bin/pfcpiface -config /opt/bess/bessctl/conf/upf.json -n4SrcIPStr 0.0.0.0 -p4RtcServerIP onos1 -p4RtcServerPort 51001
