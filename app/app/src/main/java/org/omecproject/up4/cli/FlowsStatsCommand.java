7fe752ab210ebfd60a77e6aeffe27909a186782c
Daniele Moro <daniele.moro@intel.com>
2022-04-27 16:28:20 +0200

    Address review comments

--- app/app/src/main/java/org/omecproject/up4/cli/FlowsStatsCommand.java
+++ app/app/src/main/java/org/omecproject/up4/cli/FlowsStatsCommand.java
@@ -5,6 +5,7 @@
 package org.omecproject.up4.cli;
 
 import com.google.common.collect.Maps;
+import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.omecproject.up4.impl.Up4AdminService;
@@ -23,44 +24,88 @@ import java.util.Map;
 public class FlowsStatsCommand extends AbstractShellCommand {
 
     private static final String SEPARATOR = "-".repeat(40);
-    private static final int SLEEP_TIME_MS = 200;
+    private static final int DEFAULT_SLEEP_TIME = 2000; // 2 seconds
+
+    @Argument(index = 0, name = "sleep-time",
+            description = "Time between counter reads",
+            required = false)
+    int sleepTimeMs = DEFAULT_SLEEP_TIME;
+
+    // Add completer?
+    @Argument(index = 1, name = "ue-session-id",
+            description = "UE Session ID (IPv4 UE address)",
+            required = false)
+    String ueSessionId = null;
 
     @Override
     protected void doExecute() throws Exception {
         Up4AdminService adminService = get(Up4AdminService.class);
+        Ip4Address ueAddr = ueSessionId != null ? Ip4Address.valueOf(ueSessionId) : null;
 
         Map<Ip4Address, UpfCounter> beforeDownlink = Maps.newHashMap();
-        adminService.getDownlinkFlows().forEach(dlUpfFlow -> {
-            beforeDownlink.put(dlUpfFlow.getSession().ueAddress(), dlUpfFlow.getCounter());
-        });
+        adminService.getDownlinkFlows().stream()
+                .filter(upfFlow -> ueAddr == null || ueAddr.equals(upfFlow.getTermination().ueSessionId()))
+                .forEach(
+                        dlUpfFlow -> beforeDownlink.compute(
+                                dlUpfFlow.getTermination().ueSessionId(),
+                                (key, value) -> (value == null) ?
+                                        dlUpfFlow.getCounter() :
+                                        sumUpfCounters(value, dlUpfFlow.getCounter())
+                        )
+                );
 
         Map<Ip4Address, UpfCounter> beforeUplink = Maps.newHashMap();
-        adminService.getUplinkFlows().forEach(ulUpfFlow -> {
-            beforeUplink.put(ulUpfFlow.getTermination().ueSessionId(), ulUpfFlow.getCounter());
-        });
-        Thread.sleep(SLEEP_TIME_MS);
+        adminService.getUplinkFlows().stream()
+                .filter(upfFlow -> ueAddr == null || ueAddr.equals(upfFlow.getTermination().ueSessionId()))
+                .forEach(
+                        ulUpfFlow -> beforeUplink.compute(
+                                ulUpfFlow.getTermination().ueSessionId(),
+                                (key, value) -> (value == null) ?
+                                        ulUpfFlow.getCounter() :
+                                        sumUpfCounters(value, ulUpfFlow.getCounter())
+                        )
+                );
 
-        Map<Ip4Address, UpfCounter> afterDownlink = Maps.newHashMap();
-        adminService.getDownlinkFlows().forEach(dlUpfFlow -> {
-            afterDownlink.put(dlUpfFlow.getSession().ueAddress(), dlUpfFlow.getCounter());
-        });
-
-        Map<Ip4Address, UpfCounter> afterUplink = Maps.newHashMap();
-        adminService.getUplinkFlows().forEach(ulUpfFlow -> {
-            afterUplink.put(ulUpfFlow.getTermination().ueSessionId(), ulUpfFlow.getCounter());
-        });
-
-        print(SEPARATOR);
-        if (!(beforeDownlink.keySet().equals(beforeUplink.keySet()) &&
-                beforeDownlink.keySet().equals(afterUplink) &&
-                beforeDownlink.keySet().equals(afterDownlink))) {
-            print("ERROR while reading stats!");
+        if (beforeDownlink.isEmpty() || beforeUplink.isEmpty()) {
+            print("No UE Sessions");
             return;
         }
+        Thread.sleep(sleepTimeMs);
+
+        Map<Ip4Address, UpfCounter> afterDownlink = Maps.newHashMap();
+        adminService.getDownlinkFlows().stream()
+                .filter(upfFlow -> ueAddr == null || ueAddr.equals(upfFlow.getTermination().ueSessionId()))
+                .forEach(
+                        dlUpfFlow -> afterDownlink.compute(
+                                dlUpfFlow.getTermination().ueSessionId(),
+                                (key, value) -> (value == null) ?
+                                        dlUpfFlow.getCounter() :
+                                        sumUpfCounters(value, dlUpfFlow.getCounter())
+                        )
+                );
+
+        Map<Ip4Address, UpfCounter> afterUplink = Maps.newHashMap();
+        adminService.getUplinkFlows().stream()
+                .filter(upfFlow -> ueAddr == null || ueAddr.equals(upfFlow.getTermination().ueSessionId()))
+                .forEach(
+                        ulUpfFlow -> afterUplink.compute(
+                                ulUpfFlow.getTermination().ueSessionId(),
+                                (key, value) -> (value == null) ?
+                                        ulUpfFlow.getCounter() :
+                                        sumUpfCounters(value, ulUpfFlow.getCounter())
+                        )
+                );
+
+        if (!(beforeDownlink.keySet().containsAll(beforeUplink.keySet()) &&
+                beforeDownlink.keySet().equals(afterUplink.keySet()) &&
+                beforeDownlink.keySet().equals(afterDownlink.keySet()))) {
+            print("Error while reading stats!");
+            return;
+        }
+
+        print(SEPARATOR);
         for (Ip4Address ueSessionId : beforeDownlink.keySet()) {
-            if (!beforeUplink.containsKey(ueSessionId) ||
-                    !afterDownlink.containsKey(ueSessionId) ||
-                    !afterUplink.containsKey(ueSessionId)) {
+            if (ueAddr == null || ueAddr.equals(ueSessionId)) {
                 print("UE session: " + ueSessionId.toString());
                 UpfCounter bfUl = beforeUplink.get(ueSessionId);
                 UpfCounter bfDl = beforeDownlink.get(ueSessionId);
@@ -71,38 +116,48 @@ public class FlowsStatsCommand extends AbstractShellCommand {
                 long droppedPktsDl = (afDl.getIngressPkts() - afDl.getEgressPkts()) -
                         (bfDl.getIngressPkts() - bfDl.getEgressPkts());
 
-                long txPktsUl = (afUl.getEgressPkts() - bfUl.getEgressPkts()) / SLEEP_TIME_MS;
-                long txBytesUl = (afUl.getEgressBytes() - bfUl.getEgressBytes()) / SLEEP_TIME_MS;
-                long txPktsDl = (afDl.getEgressPkts() - bfDl.getEgressPkts()) / SLEEP_TIME_MS;
-                long txBytesDl = (afDl.getEgressBytes() - bfDl.getEgressBytes()) / SLEEP_TIME_MS;
+                double txPktsUl = (afUl.getEgressPkts() - bfUl.getEgressPkts()) / (sleepTimeMs / 1000.0);
+                double txBytesUl = ((afUl.getEgressBytes() - bfUl.getEgressBytes()) * 8) / (sleepTimeMs / 1000.0);
+                double txPktsDl = (afDl.getEgressPkts() - bfDl.getEgressPkts()) / (sleepTimeMs / 1000.0);
+                double txBytesDl = ((afDl.getEgressBytes() - bfDl.getEgressBytes()) * 8) / (sleepTimeMs / 1000.0);
 
-                long rxPktsUl = (afUl.getIngressPkts() - bfUl.getIngressPkts()) / SLEEP_TIME_MS;
-                long rxBytesUl = (afUl.getIngressBytes() - bfUl.getIngressBytes()) / SLEEP_TIME_MS;
-                long rxPktsDl = (afDl.getIngressPkts() - bfDl.getIngressPkts()) / SLEEP_TIME_MS;
-                long rxBytesDl = (afDl.getIngressBytes() - bfDl.getIngressBytes()) / SLEEP_TIME_MS;
+                double rxPktsUl = (afUl.getIngressPkts() - bfUl.getIngressPkts()) / (sleepTimeMs / 1000.0);
+                double rxPktsDl = (afDl.getIngressPkts() - bfDl.getIngressPkts()) / (sleepTimeMs / 1000.0);
 
-                print("UPLINK");
-                print("    RX:  %s  %s", toReadable(rxBytesUl, "Bps"), toReadable(rxPktsUl, "pkt/s"));
-                print("    TX:  %s  %s", toReadable(txBytesUl, "Bps"), toReadable(txPktsUl, "pkt/s"));
-                print("    DROPPED:  %d", droppedPktsDl);
-                print("DOWNLINK");
-                print("    RX:  %s  %s", toReadable(rxBytesDl, "Bps"), toReadable(rxPktsDl, "pkt/s"));
-                print("    TX:  %s  %s", toReadable(txBytesDl, "Bps"), toReadable(txPktsDl, "pkt/s"));
-                print("    DROPPED:  %d", droppedPktsUl);
+                print("Uplink");
+                print("  tput: %s / %s (%.1f%% dropped)",
+                      toReadable(txBytesUl, "bps"),
+                      toReadable(txPktsUl, "pps"),
+                      rxPktsUl == 0 ? 0.0 : (droppedPktsUl / rxPktsUl) * 100.0);
+                print("Downlink");
+                print("  tput: %s / %s (%.1f%% dropped)",
+                      toReadable(txBytesDl, "bps"),
+                      toReadable(txPktsDl, "pps"),
+                      rxPktsDl == 0 ? 0.0 : (droppedPktsDl / rxPktsDl) * 100.0);
                 print(SEPARATOR);
             }
         }
     }
 
-    String toReadable(long value, String unit) {
+    private String toReadable(double value, String unit) {
         if (value < 1000) {
-            return String.format("%.1f %s", (float) value, unit);
+            return String.format("%.3f %s", value, unit);
         } else if (value < 1000000) {
-            return String.format("%.1f K%s", value / 1000.0, unit);
+            return String.format("%.3f K%s", value / 1000.0, unit);
         } else if (value < 1000000000) {
-            return String.format("%.1f M%s", value / 1000000.0, unit);
+            return String.format("%.3f M%s", value / 1000000.0, unit);
         } else {
-            return String.format("%.1f G%s", value / 1000000000.0, unit);
+            return String.format("%.3f G%s", value / 1000000000.0, unit);
         }
+    }
+
+    private UpfCounter sumUpfCounters(UpfCounter first, UpfCounter second) {
+        return UpfCounter.builder()
+                .setIngress(first.getIngressPkts() + second.getIngressPkts(),
+                            first.getIngressBytes() + second.getIngressBytes())
+                .setEgress(first.getEgressPkts() + second.getEgressPkts(),
+                           first.getEgressBytes() + second.getEgressBytes())
+                .withCellId(first.getCellId())
+                .build();
     }
 }
